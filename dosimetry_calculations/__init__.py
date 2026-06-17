9b531c42bfede968b93122ae4c4618d1a2fab72e
albama45 <alba.martinez.simarro@gmail.com>
2026-06-17 19:20:26 +0200

    Import libraries in __init__.py
    Added necessary libraries for dosimetry calculations.

--- dosimetry_calculations/__init__.py
+++ dosimetry_calculations/__init__.py
@@ -1,1 +1,14 @@
+#libraries
 
+!pip install pydicom
+import os
+import numpy as np
+import pydicom
+import matplotlib.pyplot as plt
+import sys
+import glob
+import pandas as pd
+
+import pickle
+from google.colab import files
+from scipy.ndimage import convolve
